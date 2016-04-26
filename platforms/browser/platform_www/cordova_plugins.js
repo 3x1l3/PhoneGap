e3a4c7812cc16f20438af2fb098aee5094bf4cbd
Chad <chadklassen1@gmail.com>
2016-04-26 09:36:33 -0600

    Revert "Added push plugin"
    This reverts commit 561def397a12f7a324bb62d16216c1887b58815f.

--- platforms/browser/platform_www/cordova_plugins.js
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -15,22 +15,13 @@ module.exports = [
         "merges": [
             "navigator.notification"
         ]
-    },
-    {
-        "file": "plugins/com.phonegap.plugins.PushPlugin/www/PushNotification.js",
-        "id": "com.phonegap.plugins.PushPlugin.PushNotification",
-        "pluginId": "com.phonegap.plugins.PushPlugin",
-        "clobbers": [
-            "PushNotification"
-        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "cordova-plugin-whitelist": "1.2.1",
-    "cordova-plugin-dialogs": "1.2.1",
-    "com.phonegap.plugins.PushPlugin": "2.5.0"
+    "cordova-plugin-dialogs": "1.2.1"
 }
 // BOTTOM OF METADATA
 });
