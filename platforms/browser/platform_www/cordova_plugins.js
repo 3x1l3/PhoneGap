f7199238c77ac2effc2be862bf8230fa235cf7b7
Chad <chadklassen1@gmail.com>
2016-04-26 10:35:11 -0600

    Added push lib, have required sdk installed.

--- platforms/browser/platform_www/cordova_plugins.js
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -15,13 +15,22 @@ module.exports = [
         "merges": [
             "navigator.notification"
         ]
+    },
+    {
+        "file": "plugins/phonegap-plugin-push/www/push.js",
+        "id": "phonegap-plugin-push.PushNotification",
+        "pluginId": "phonegap-plugin-push",
+        "clobbers": [
+            "PushNotification"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "cordova-plugin-whitelist": "1.2.1",
-    "cordova-plugin-dialogs": "1.2.1"
+    "cordova-plugin-dialogs": "1.2.1",
+    "phonegap-plugin-push": "1.6.2"
 }
 // BOTTOM OF METADATA
 });
